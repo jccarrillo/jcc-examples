a6194684cd40c557c96bda9dc9d79b5546561e6e
JC Carrillo <jcarrillo@coruscant.local>
2016-02-21 10:36:51 -0500

    Use javax.inject

--- rest/echo/spring-boot/src/main/java/org/jcc/examples/spring/controller/EchoController.java
+++ rest/echo/spring-boot/src/main/java/org/jcc/examples/spring/controller/EchoController.java
@@ -2,10 +2,11 @@ package org.jcc.examples.spring.controller;
 
 import org.jcc.examples.echo.model.Echo;
 import org.jcc.examples.echo.service.EchoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 import static org.jcc.examples.echo.config.UrlPath.PATH_VARIABLE_ECHO;
 import static org.jcc.examples.echo.config.UrlPath.URL_ECHO;
@@ -13,7 +14,7 @@ import static org.jcc.examples.echo.config.UrlPath.URL_ECHO;
 @RestController
 public class EchoController {
 
-    @Autowired
+    @Inject
     private EchoService echoService;
 
     @RequestMapping(URL_ECHO)
